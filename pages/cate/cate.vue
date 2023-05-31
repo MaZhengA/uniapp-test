e185500295b554831b605ff98348fb9fa44420c7
MaZhengA <17826817363@163.com>
2023-05-31 21:15:46 +0800

    second

---
+++ pages/cate/cate.vue
@@ -0,0 +1,22 @@
+<template>
+  <view>
+    Cate
+  </view>
+</template>
+
+<script>
+  export default {
+    data() {
+      return {
+        
+      }
+    },
+    methods: {
+      
+    }
+  }
+</script>
+
+<style>
+
+</style>
