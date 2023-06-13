6b34acc6fcbaac4be7c30ccc5ad0f3a561fb0145
MaZhengA <17826817363@163.com>
2023-06-13 19:37:22 +0800

    merge

---
+++ components/my-goods/my-goods.vue
@@ -0,0 +1,58 @@
+<template>
+ <view class="goods-item">
+   <view class="goods-item-left">
+     <image :src="goods.goods_small_logo || defaultPic" class="goods-pic"></image>
+   </view>
+   <view class="goods-item-right">
+     <view class="goods-name">{{ goods.goods_name }}</view>
+     <view class="goods-info-box">
+       <view class="goods-price">¥ {{ goods.goods_price | tofixed }}</view>
+     </view>
+   </view>
+ </view>
+</template>
+
+<script>
+  export default {
+    name:"my-goods",
+    props: {
+      goods: {
+        type: Object,
+        default: {}
+      }
+    },
+    data() {
+      return {
+        defaultPic: 'http://image4.suning.cn/uimg/b2c/newcatentries/0000000000-000000000185518945_2_800x800.jpg'
+      };
+    },
+    filters: {
+      tofixed(num) {
+        return Number(num).toFixed(2)
+      }
+    }
+  }
+</script>
+
+<style lang="scss">
+.goods-item {
+  display: flex;
+  padding: 10px;
+  height: 100px;
+  border: 1px solid #dfdfdf;
+}
+.goods-pic {
+  width: 140px;
+  height: 100%;
+}
+.goods-item-right {
+  display: flex;
+  flex-direction: column;
+  justify-content: space-between;
+  padding-left: 10px;
+}
+.goods-info-box {
+  font-size: 20px;
+  color: red;
+}
+</style>
--- subpkg/goods_list/goods_list.vue
+++ subpkg/goods_list/goods_list.vue
@@ -1,6 +1,11 @@
 <template>
   <view>
-    goods_list
+    <view class="goods-list">
+      <view v-for="(item, index) in goodsList" :key="index" @click="gotoDetail(item)">
+        <!-- 通过组件的方式渲染item项 -->
+        <my-goods :goods="item"></my-goods>
+      </view>
+    </view>
   </view>
 </template>
 
@@ -8,12 +13,53 @@
   export default {
     data() {
       return {
-        
+        queryObj: {
+          query: '',
+          cid: '',
+          pagenum: 1,
+          pagesize: 10
+        },
+        goodsList: [],
+        total: 0
       };
+    },
+    onLoad(options) {
+      this.queryObj.query = options.query || '';
+      this.queryObj.cid = options.cid || '';
+      this.getGoodsList();
+    },
+    methods: {
+      async getGoodsList(cb) {
+        const {data: res} = await uni.$http.get('/api/public/v1/goods/search', this.queryObj);
+        // 数据请求完毕后，就立即按需调用cb函数
+        cb && cb();
+        if (res.meta.status !== 200)  return umi.$showMsg()
+        // uni.$showMsg('数据请求成功')
+        this.goodsList = [...this.goodsList, ...res.message.goods];
+        this.total = res.total;
+      },
+      async gotoDetail(item) {
+        uni.navigateTo({
+          url: "/subpkg/goods_detail/goods_detail?goods_id=" + item.goods_id
+        })
+      }
+    },
+    onReachBottom() {
+      if (this.queryObj.pagenum * this.queryObj.pagesize >= this.total) return uni.$showMsg('数据加载完毕')
+      this.queryObj.pagenum += 1;
+      this.getGoodsList();
+    },
+    onPullDownRefresh() {
+      this.queryObj.pagenum = 1;
+      this.total = 0;
+      this.goodsList = [];
+      this.getGoodsList(() => uni.stopPullDownRefresh())
     }
   }
 </script>
 
 <style lang="scss">
-
+.goods-list {
+  
+}
 </style>
